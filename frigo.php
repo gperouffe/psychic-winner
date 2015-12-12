802c7001c123a595863d3951fe7cf10a28ae9080
Guillaume Perouffe <guillaume.perouffe@centrale-marseille.fr>
2015-12-12 14:12:18 +0100

    Remplissage auto contenu page frigo avec donnees BDD

--- frigo.php
+++ frigo.php
@@ -25,34 +25,27 @@
 		
 		<div class="row">
 			<form action="resultats.php" method="post">
-				<div class="col s12 m6 l2" >
-					<h4>Viande</h4>
+				<?php
+				$db = new SQLite3("db/cuisineEtudiante.db");
+				$listeTypes = $db->query('SELECT * FROM alimentcat');
+				while($type = $listeTypes->fetchArray()){
+				echo
+				'<div class="col s12 m6 l2" >
+					<h4>'.$type[1].'</h4>';
 					
-					<?php
-					$db = new SQLite3("db/cuisineEtudiante.db");
-					$reponse = $db->query('SELECT * FROM ingredients WHERE TYPE = 1');
+					$reponse = $db->query('SELECT * FROM ingredients WHERE TYPE = '.$type[0]);
 					while($tuple = $reponse->fetchArray()){
 					echo
 					'<p>
-						<input type="checkbox" class="filled-in" id="'.$tuple[1].'" name="viande" value="'.$tuple[1].'" />
+						<input type="checkbox" class="filled-in" id="'.$tuple[1].'" name="'.$type[1].'" value="'.$tuple[1].'" />
 						<label class="black-text" for="'.$tuple[1].'">'.$tuple[1].'</label>
 					</p>';
 					}
-					?>
-					
-				</div>
-				<div class="col s12 m6 l2">
-					<h4>Légumes</h4>
-					<p>
-						<input type="checkbox" class="filled-in" id="poivrons" name="légumes" value="poivrons" />
-						<label class="black-text" for="poivrons">Poivrons</label>
-					</p>
-					<p>
-						<input type="checkbox" class="filled-in" id="courgettes" name="légumes" value="courgettes" />
-						<label class="black-text" for="courgettes">Courgettes</label>
-					</p>
-					
-				</div>
+				
+				echo
+				'</div>';
+				}
+				?>
 				<p><input type="submit" name="Appuie !"/></p>
 			</form>
 		</div>
